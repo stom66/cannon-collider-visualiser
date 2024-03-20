cfebabb74a0f32962a3f2931052d3be4f9ddec29
stom <tomsteventon@gmail.com>
2024-03-20 19:40:20 +0000

    cleanup: remove unused property from itnerface

--- src/interface.Collider.ts
+++ src/interface.Collider.ts
@@ -1,15 +1,14 @@
 export interface Collider {
-	obj_name       : string,
-	position       : number[],
-	type           : string,
-	shape          : string,
-	friction       : number,
-	restitution    : number,
-	mass           : number,
-	radius?        : number,   // Only present for SPHERE colliders
-	dimensions?    : number[], // Only present for BOX colliders
-	rotation?      : number[], // Only present for BOX colliders - is quaternion
-	rotation_order?: string,   // Only present for BOX colliders
-	vertices?      : number[], // Only present for MESH colliders
-	indices?       : number[], // Only present for MESH colliders
+	obj_name: string,
+	position: number[],
+	type: string,
+	shape: string,
+	friction: number,
+	restitution: number,
+	mass: number,
+	radius?: number,   // Only present for SPHERE colliders
+	dimensions?: number[], // Only present for BOX colliders
+	rotation?: number[], // Only present for BOX colliders - is quaternion
+	vertices?: number[], // Only present for MESH colliders
+	indices?: number[], // Only present for MESH colliders
 }
